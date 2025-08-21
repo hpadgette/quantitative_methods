f6635fbd0d72f74abfe5fd4429942420a8a7ff9f
hpadgette <76794472+hpadgette@users.noreply.github.com>
2025-08-21 16:29:23 -0600

    Add files via upload

---
+++ Homework 4/HW4_HPadgette_SPYDER.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 13:13:49 2023
+
+@author: hannahpadgette
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+%reset -f
+plt.close('all')
+
+x = np.linspace(0, 3 * np.pi / 2, 100)
+
+help(plt.subplots)
+
+help(plt.title)
